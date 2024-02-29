11b6f23248036baad7bfca01533e7dd41099a567
Emma-Ez <emma.favoured@yahoo.com>
2024-02-29 10:36:32 +0100

    Edit and delete icons added

--- to-do.js
+++ to-do.js
@@ -14,12 +14,29 @@ function addTask() {
     }
     else{
         let li = document.createElement("li");
-        li.innerHTML = textBox.value;
+        li.textContent = textBox.value;
         listContainer.appendChild(li);
+        // let span = document.createElement("span");
+        // span.innerHTML = `<div><img class="edit text-controls" src="/images/pencil.png" alt="Edit task" /><img class="delete text-controls" src="/images/delete.png" alt="Delete task" /></div>`;
+        // li.appendChild(span);
+        let span = document.createElement("span");
+        span.textContent = " ";
+        const editIcon = document.createElement("img");
+        editIcon.classList.add("edit", "text-controls");
+        editIcon.src = "/images/pencil.png";
+        editIcon.alt = "Edit task";
+        span.appendChild(editIcon);
 
+        const deleteIcon = document.createElement("img");
+        deleteIcon.classList.add("delete", "text-controls");
+        deleteIcon.src = "/images/delete.png";
+        deleteIcon.alt = "Delete task";
+        span.appendChild(deleteIcon);
+
+        li.appendChild(span);
     }
     textBox.value = "";
-
+    
 }
 
 listContainer.addEventListener("click", function(e) {
